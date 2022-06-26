89031ebb0a8821bb3930e49bccdaacd4d063a862
William Snider <williamsnider@Williams-MacBook-Pro.local>
2022-06-25 20:53:04 -0400

    GeNN GLIF5

--- GLIF_models.py
+++ GLIF_models.py
@@ -181,9 +181,96 @@ GLIF4 = create_custom_neuron_class(
     """,
     threshold_condition_code="$(V) > $(th_inf) + $(th_s)",
     reset_code="""
-    $(V)=0;
+    $(V)= $(El) + $(a) * ($(V) - $(El)) + $(b);
     $(ASC)[0] = $(asc_amp_array)[0] + $(ASC)[0] * $(r)[0] * exp(-($(k)[0] * DT * $(spike_cut_length)));
     $(ASC)[1] = $(asc_amp_array)[1] + $(ASC)[1] * $(r)[1] * exp(-($(k)[1] * DT * $(spike_cut_length)));
+    $(th_s) = $(th_s) * exp(-$(b_spike)*DT);
+    $(refractory_count) = $(spike_cut_length);
+    """,
+)
+
+GLIF5 = create_custom_neuron_class(
+    "GLIF5",
+    param_names=[
+        "C",
+        "G",
+        "El",
+        "a",
+        "b",
+        "a_spike",
+        "b_spike",
+        "spike_cut_length",
+        "th_inf",
+        "a_voltage",
+        "b_voltage",
+    ],
+    var_name_types=[
+        ("V", "double"),
+        ("refractory_count", "int"),
+        ("th_s", "double"),
+        ("th_v", "double")
+    ],
+    extra_global_params=[
+        ("ASC", "scalar*"),
+        ("k", "scalar*"),
+        ("asc_amp_array", "scalar*"),
+        ("r", "scalar*"),
+    ],
+    sim_code="""
+    
+    double sum_of_ASC;
+    double I;
+    double beta;
+    double phi;
+
+    // Threshold spike and voltage components
+    if ($(refractory_count) == 1) {
+        $(th_s) = $(th_s) * exp(-$(b_spike)*DT) + $(a_spike);
+    }
+    else {
+        $(th_s) = $(th_s) * exp(-$(b_spike)*DT);
+    }
+
+    // Must come before ASC updated
+    if ($(refractory_count) <= 0) {
+        sum_of_ASC = $(ASC)[0] + $(ASC)[1];
+        I = $(Isyn) + sum_of_ASC;
+        beta = (I+$(G)*$(El))/$(G);
+        phi = $(a_voltage)/($(b_voltage)-$(G)/$(C));
+        $(th_v) = phi*($(V)-beta)*exp(-$(G)*DT/$(C))+1/(exp($(b_voltage)*DT))*($(th_v)-phi*($(V)-beta)-($(a_voltage)/$(b_voltage))*(beta-$(El))-0) +($(a_voltage)/$(b_voltage))*(beta-$(El));
+    } else {
+        $(th_v) += 0.0;
+    }
+
+    // Refractory period dynamics
+    if ($(refractory_count) > 0) {
+        $(V) += 0.0;
+        $(ASC)[0] += 0.0;
+        $(ASC)[1] += 0.0;
+        }
+
+    // Normal dynamics
+    else {
+        // Voltage 
+        sum_of_ASC = $(ASC)[0] + $(ASC)[1];
+        $(V)+=1/$(C)*($(Isyn)+sum_of_ASC-$(G)*($(V)-$(El)))*DT;
+        
+        // ASCurrents
+        $(ASC)[0] = $(ASC)[0] * exp(-$(k)[0]*DT);
+        $(ASC)[1] = $(ASC)[1] * exp(-$(k)[1]*DT);
+    }
+
+    // Decrement refractory_count by 1; Do not decrement past -1
+    if ($(refractory_count) > -1) {
+        $(refractory_count) -= 1;
+    }
+    """,
+    threshold_condition_code="$(V) > $(th_inf) + $(th_s) + $(th_v)",
+    reset_code="""
+    $(V)= $(El) + $(a) * ($(V) - $(El)) + $(b);
+    $(ASC)[0] = $(asc_amp_array)[0] + $(ASC)[0] * $(r)[0] * exp(-($(k)[0] * DT * $(spike_cut_length)));
+    $(ASC)[1] = $(asc_amp_array)[1] + $(ASC)[1] * $(r)[1] * exp(-($(k)[1] * DT * $(spike_cut_length)));
+    $(th_v) = $(th_v);
     $(th_s) = $(th_s) * exp(-$(b_spike)*DT);
     $(refractory_count) = $(spike_cut_length);
     """,
---
+++ compare_Allen_and_GeNN/GeNN_GLIF5.py
@@ -0,0 +1,147 @@
+import sys
+
+sys.path.append("..")  # TODO: Come up with a better way to handle the imports
+from pygenn.genn_model import create_custom_current_source_class, GeNNModel
+from allen_simulation import load_model_config_stimulus
+from GLIF_models import GLIF5
+from parameters import GLIF_dict
+import numpy as np
+from pathlib import Path
+from utilities import (
+    plot_results_and_diff,
+    check_nan_arrays_equal,
+    count_unequal_ignoring_nans,
+)
+
+specimen_ids = [474637203]  # , 512322162]
+model_types = ["LIFRASCAT_model"]
+saved_models = [f.name for f in Path("./pkl_data").glob("*")]
+
+for specimen_id in specimen_ids:
+    for model_type in model_types:
+
+        # Load Allen model parameters
+        saved_model, config, stimulus = load_model_config_stimulus(
+            specimen_id, model_type
+        )
+
+        # Convert Allen Units (SI) to PyGenn
+        units_dict = {
+            "C": config["C"] * 1e9,  # F -> nF
+            "G": 1 / config["R_input"] * 1e6,  # S -> uS
+            "El": config["El"] * 1e3,  # V -> mV
+            "th_inf": config["th_inf"] * config["coeffs"]["th_inf"] * 1e3,  # V -> mV
+            "dT": config["dt"] * 1e3,  # s -> ms
+            "V": config["init_voltage"] * 1e3,  # V -> mV,
+            "spike_cut_length": config["spike_cut_length"],
+            "a": config["voltage_reset_method"]["params"]["a"],
+            "b": config["voltage_reset_method"]["params"]["b"] * 1e3,  # V -> mV
+            "a_spike": config["threshold_dynamics_method"]["params"]["a_spike"]
+            * 1e3,  # V -> mV
+            "b_spike": config["threshold_dynamics_method"]["params"]["b_spike"]
+            * 1e-3,  # inverse of s -> ms
+            "refractory_count": -1,
+            "k": 1 / np.array(config["asc_tau_array"]) * 1e-3,  # inverse of s -> ms
+            "r": np.array(config["AScurrent_reset_method"]["params"]["r"]),
+            "ASC": np.array(config["init_AScurrents"]) * 1e9,  # A -> nA
+            "asc_amp_array": np.array(config["asc_amp_array"])
+            * np.array(config["coeffs"]["asc_amp_array"])
+            * 1e9,  # A -> nA
+            "th_s": 0.0 * 1e3,  # V -> mV
+            "th_v": 0.0 * 1e3,  # V -> mV
+            "a_voltage":  config['threshold_dynamics_method']["params"]["a_voltage"] * config["coeffs"]["a"] * 1e-3, # inverse of V -> mV
+            "b_voltage" : config['threshold_dynamics_method']["params"]["b_voltage"] * config["coeffs"]["b"] * 1e-3, # inverse of V -> mV
+        }
+
+        # Add GLIF Class to model
+        num_neurons = 1
+        GLIF = eval(GLIF_dict[model_type])
+        GLIF_params = {k: units_dict[k] for k in GLIF.get_param_names()}
+        GLIF_init = {k: units_dict[k] for k in ["V", "refractory_count", "th_s", "th_v"]}
+        model = GeNNModel("double", GLIF_dict[model_type], backend="SingleThreadedCPU")
+        model.dT = units_dict["dT"]
+        pop1 = model.add_neuron_population(
+            pop_name="pop1",
+            num_neurons=num_neurons,
+            neuron=GLIF,
+            param_space=GLIF_params,
+            var_space=GLIF_init,
+        )
+
+        # Assign extra global parameter values
+        for k in pop1.extra_global_params.keys():
+            pop1.set_extra_global_param(k, units_dict[k])
+
+        # pop1.set_extra_global_param("ASC", )
+        # pop1.extra_global_params
+        ### Add current source to model ###
+        external_current_source = create_custom_current_source_class(
+            class_name="external_current",
+            var_name_types=[("current", "double")],
+            injection_code="""
+            $(current)=$(Ie)[int($(t) / DT)];
+            $(injectCurrent,$(current) );
+            """,
+            extra_global_params=[("Ie", "double*")],
+        )
+        cs_ini = {"current": 0.0}  # external input current from Teeter 2018
+        cs = model.add_current_source(
+            cs_name="external_current_source",
+            current_source_model=external_current_source,
+            pop=pop1,
+            param_space={},
+            var_space=cs_ini,
+        )
+
+        scale = 1e9  # amps -> nanoamps
+        cs.set_extra_global_param("Ie", stimulus * scale)
+
+        model.build()
+        model.load()
+
+        # Run a quick simulation
+        num_steps = len(stimulus)
+        v = np.empty((num_steps, num_neurons))
+        v_view = pop1.vars["V"].view
+        T = np.ones((num_steps, num_neurons)) * units_dict["th_inf"]
+        th_s_view = pop1.vars["th_s"].view
+        th_v_view = pop1.vars["th_v"].view
+        A = np.empty((num_steps, num_neurons, len(units_dict["ASC"])))
+        A_view = pop1.extra_global_params["ASC"].view
+        for i in range(num_steps):
+            model.step_time()
+            pop1.pull_var_from_device("V")
+            v[model.timestep - 1, :] = v_view[:]
+            A[model.timestep - 1, :, :] = A_view[:]
+            # pop1.pull_var_from_device("th_s")
+            T[model.timestep - 1, :] += th_s_view[:] + th_v_view[:]
+
+        t = saved_model["time"]
+        mask = np.logical_and(t > 18, t < 18.3)
+        t_mask = t[mask]
+
+        # Plot voltages
+        Allen = saved_model["voltage"][mask] * 1e3
+        GeNN = v[mask, :].ravel()
+        result = check_nan_arrays_equal(Allen, GeNN)
+        print("Are results equal: {}".format(result))
+        plot_results_and_diff(Allen, "Allen", GeNN, "GeNN", t[mask])
+
+        # Plot thresholds
+        Allen = saved_model["threshold"][mask] * 1e3
+        GeNN = T[mask, :].ravel()
+        result = check_nan_arrays_equal(Allen, GeNN)
+        print("Are results equal: {}".format(result))
+        plot_results_and_diff(Allen, "Allen", GeNN, "GeNN", t[mask])
+
+        # Plot ASCurrents
+        Allen = saved_model["AScurrents"][mask] * 1e9  # A --> nA
+        GeNN = np.squeeze(A)[mask, :]
+        result = check_nan_arrays_equal(Allen, GeNN)
+        print("Are ASCurrents results equal: {}".format(result))
+        print(
+            "{} values differed between ASCurrents".format(
+                count_unequal_ignoring_nans(Allen, GeNN)
+            )
+        )
+        plot_results_and_diff(Allen, "Allen", GeNN, "GeNN", t[mask])
